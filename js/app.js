940054273a3adbdceb0b56ed9eefa000a313ff7b
Alex G Mendoza <alexgm511@gmail.com>
2015-03-19 13:34:01 -0700

    Added array of cats and functionality to change view to each one of the cats

--- js/app.js
+++ js/app.js
@@ -1,9 +1,55 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('images/red.jpg');
-	this.imgAttribution = ko.observable('http://www.alexgmendoza.com');
-	this.nicknames = ko.observableArray(["Gray", "Dusk", "Gray", "Red", "Misty", "Blondie"]);
+var initialCats = [ 
+		{
+			clickCount: 0,
+			name: "Gray", 
+			imgSrc: "images/gray.jpg",
+			imgAttribution: "http://www.alexgmendoza.com",
+			nicknames: ["G-strong", "Grayson"]
+		},
+		{
+			clickCount: 0,
+			name: "Dusk",
+			imgSrc: "images/dusk.jpg",
+			imgAttribution: "http://www.alexgmendoza.com",
+			nicknames: ["Dusker", "D-dude", "Dussie"]
+		},
+		{
+			clickCount: 0,
+			name: "Red",
+			imgSrc: "images/red.jpg",
+			imgAttribution: "http://www.alexgmendoza.com",
+			nicknames: ["Reddish", "Redster"]
+		},
+		{
+			clickCount: 0,
+			name: "Misty",
+			imgSrc: "images/misty.jpg",
+			imgAttribution: "http://www.alexgmendoza.com",
+			nicknames: ["Missie", "Mister"]
+		},
+		{
+			clickCount: 0,
+			name: "Blondie",
+			imgSrc: "images/blondie.jpg",
+			imgAttribution: "http://www.alexgmendoza.com",
+			nicknames: ["B-line", "Airhead"]
+		},
+		{
+			clickCount: 0,
+			name: "Chocolate",
+			imgSrc: "images/chocolate.jpg",
+			imgAttribution: "http://www.alexgmendoza.com",
+			nicknames: ["C-real", "Chockie"]
+		}
+];
+
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observableArray(data.nicknames);
 	
 	this.title = ko.computed(function(){
 		var title;
@@ -24,11 +70,23 @@ var Cat = function() {
 };	
 	
 var ViewModel = function() {
-	this.currentCat = ko.observable(new Cat());
+	var self = this;
+	
+	this.catList = ko.observableArray([]);
+	
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem) );
+	});
+	
+	this.currentCat = ko.observable( this.catList()[0] );
+	
+	self.changeCat = function(thisCat) {
+		//console.log(self.currentCat());
+		self.currentCat(thisCat);
+	}
 	
 	this.incrementCounter = function() {
-		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
-		//this.clickCount(this.clickCount() + 1);
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
 };
 
